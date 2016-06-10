7ac5d18aaf8debb08ee6879b839298edd5e3d2ff
Kevin Stone <kevinastone@gmail.com>
2016-06-09 19:18:57 -0700

    Added unit tests for headers endpoints

--- src/app/headers.rs
+++ src/app/headers.rs
@@ -27,3 +27,45 @@ pub fn response_headers(req: &mut Request) -> IronResult<Response> {
 
     Ok(res)
 }
+
+#[cfg(test)]
+mod test {
+
+    extern crate iron_test;
+    extern crate time;
+
+    use super::super::app;
+    use iron::Headers;
+    use self::iron_test::{request, response};
+
+    #[test]
+    fn test_headers() {
+
+        let app = app();
+
+        let mut headers = Headers::new();
+        headers.set_raw("X-Request-ID", vec![String::from("1234").into_bytes()]);
+
+        let res = request::get("http://localhost:3000/headers", headers, &app).unwrap();
+
+        let result_body = response::extract_body_to_string(res);
+        assert!(result_body.contains("X-Request-ID: 1234"))
+    }
+
+    #[test]
+    fn test_response_headers() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/response-headers?X-Request-ID=1234",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+        assert_eq!(res.headers
+                       .get_raw("X-Request-ID")
+                       .map(|v| &v[0])
+                       .and_then(|v| String::from_utf8(v.clone()).ok())
+                       .unwrap(),
+                   "1234")
+    }
+}
