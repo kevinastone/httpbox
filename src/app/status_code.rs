55b875f50be744a000c39e8b0658f31aca27da2b
Kevin Stone <kevinastone@gmail.com>
2016-06-09 21:44:55 -0700

    Added unit tests for status code and user agent endpoints

--- src/app/status_code.rs
+++ src/app/status_code.rs
@@ -12,3 +12,24 @@ pub fn status_code(req: &mut Request) -> IronResult<Response> {
 
     Ok(Response::with(status::Status::from_u16(code)))
 }
+
+#[cfg(test)]
+mod test {
+
+    extern crate iron_test;
+
+    use super::super::app;
+    use iron::Headers;
+    use iron::status;
+    use self::iron_test::request;
+
+    #[test]
+    fn test_status_code() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/status/429", Headers::new(), &app).unwrap();
+
+        assert_eq!(res.status.unwrap(), status::TooManyRequests);
+    }
+}
--- src/app/user_agent.rs
+++ src/app/user_agent.rs
@@ -9,3 +9,41 @@ pub fn user_agent(req: &mut Request) -> IronResult<Response> {
     let user_agent = iexpect!(req.headers.get::<UserAgent>());
     Ok(Response::with((status::Ok, user_agent.to_string())))
 }
+
+#[cfg(test)]
+mod test {
+
+    extern crate iron_test;
+
+    use super::super::app;
+    use super::iron::headers;
+    use iron::Headers;
+    use self::iron_test::{request, response};
+
+    #[test]
+    fn test_user_agent() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/user-agent", Headers::new(), &app).unwrap();
+
+        let result_body = response::extract_body_to_string(res);
+        assert_eq!(result_body, "iron-test")
+    }
+
+    // Broken because iron-test overrides the user-agent
+    #[test]
+    #[ignore]
+    fn test_user_agent_custom() {
+
+        let app = app();
+
+        let mut headers = Headers::new();
+        headers.set(headers::UserAgent("CustomAgent/1.0".to_owned()));
+
+        let res = request::get("http://localhost:3000/user-agent", headers, &app).unwrap();
+
+        let result_body = response::extract_body_to_string(res);
+        assert_eq!(result_body, "CustomAgent/1.0")
+    }
+}
