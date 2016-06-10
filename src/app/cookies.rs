180f43bf4b3bf72af9c83d6d8368d7e0b86ee5dc
Kevin Stone <kevinastone@gmail.com>
2016-06-09 18:42:49 -0700

    Added cookies endpoints unit tests

--- src/app/cookies.rs
+++ src/app/cookies.rs
@@ -34,3 +34,58 @@ pub fn set_cookies(req: &mut Request) -> IronResult<Response> {
 
     Ok(Response::with((status::Ok, Header(cookies))))
 }
+
+#[cfg(test)]
+mod test {
+
+    extern crate iron_test;
+    extern crate time;
+
+    use super::super::app;
+    use super::iron::headers;
+    use iron::Headers;
+    use self::iron_test::{request, response};
+
+    #[test]
+    fn test_no_cookies() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/cookies", Headers::new(), &app).unwrap();
+
+        let result_body = response::extract_body_to_string(res);
+        assert_eq!(result_body, "")
+    }
+
+    #[test]
+    fn test_cookies() {
+
+        let app = app();
+
+        let mut headers = Headers::new();
+        headers.set(headers::Cookie(vec![headers::CookiePair::new("test".to_owned(),
+                                                                  "value".to_owned())]));
+
+        let res = request::get("http://localhost:3000/cookies", headers, &app).unwrap();
+
+        let result_body = response::extract_body_to_string(res);
+        assert_eq!(result_body, "test=value")
+    }
+
+    #[test]
+    fn test_set_cookies() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/cookies/set?test=value",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+
+        let cookies = res.headers.get::<headers::SetCookie>().unwrap();
+        let cookie = cookies.0.first().unwrap();
+
+        assert_eq!(cookie.name, "test");
+        assert_eq!(cookie.value, "value");
+    }
+}
