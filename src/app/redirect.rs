af199bd0bbd004983f335f8f7af8fb6960e7b8d0
Kevin Stone <kevinastone@gmail.com>
2016-06-09 19:44:35 -0700

    Added unit tests for redirect endpoints

--- src/app/redirect.rs
+++ src/app/redirect.rs
@@ -66,7 +66,7 @@ pub fn absolute(req: &mut Request) -> IronResult<Response> {
     let url = if code <= 0 {
         String::from("/")
     } else {
-        format!("/relative-redirect/{}", code)
+        format!("/absolute-redirect/{}", code)
     };
 
     let url = iexpect!(req.url
@@ -79,4 +79,123 @@ pub fn absolute(req: &mut Request) -> IronResult<Response> {
                        status::BadRequest);
 
     Ok(Response::with((status::Found, Redirect(url))))
+}
+
+#[cfg(test)]
+mod test {
+
+    extern crate iron_test;
+
+    use super::super::app;
+    use super::iron::headers;
+    use iron::Headers;
+    use iron::status;
+    use self::iron_test::request;
+
+    #[test]
+    fn test_redirect_to() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/redirect-to?url=http://example.com",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        // TODO: Fix with new version of Iron that doesn't include the port
+        assert_eq!(location.0, "http://example.com:80/")
+    }
+
+    #[test]
+    fn test_redirect() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/redirect/5", Headers::new(), &app).unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        assert_eq!(location.0, "/relative-redirect/4")
+    }
+
+    #[test]
+    fn test_redirect_last() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/redirect/1", Headers::new(), &app).unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        assert_eq!(location.0, "/")
+    }
+
+    #[test]
+    fn test_relative_redirect() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/relative-redirect/5",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        assert_eq!(location.0, "/relative-redirect/4")
+    }
+
+    #[test]
+    fn test_relative_redirect_last() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/relative-redirect/1",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        assert_eq!(location.0, "/")
+    }
+
+    #[test]
+    fn test_absolute_redirect() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/absolute-redirect/5",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        assert_eq!(location.0, "http://localhost:3000/absolute-redirect/4")
+    }
+
+    #[test]
+    fn test_absolute_redirect_last() {
+
+        let app = app();
+
+        let res = request::get("http://localhost:3000/absolute-redirect/1",
+                               Headers::new(),
+                               &app)
+            .unwrap();
+
+        assert_eq!(res.status.unwrap(), status::Found);
+
+        let location = res.headers.get::<headers::Location>().unwrap();
+        assert_eq!(location.0, "http://localhost:3000/")
+    }
 }
