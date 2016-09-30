7cec4baa8aea933bab2b2edea2a25bed9f08d2ac
Kevin Stone <kevinastone@gmail.com>
2016-09-30 16:12:15 -0700

    Cleaned up const

--- src/app/ip.rs
+++ src/app/ip.rs
@@ -4,10 +4,12 @@ use self::iron::{Request, Response, IronResult};
 use self::iron::headers;
 use self::iron::status;
 
+pub const X_FORWARD_FOR: &'static str = "X-Forwarded-For";
+
 
 pub fn ip(req: &mut Request) -> IronResult<Response> {
     let remote_ip = iexpect!(req.headers
-        .get_raw("X-Forwarded-For")
+        .get_raw(X_FORWARD_FOR)
         .and_then(|h| headers::parsing::from_one_raw_str(h).ok())
         .or_else(|| Some(req.remote_addr.ip().to_string())));
 
@@ -19,6 +21,7 @@ mod test {
 
     extern crate iron_test;
 
+    use super::X_FORWARD_FOR;
     use super::super::app;
     use iron::Headers;
     use self::iron_test::{request, response};
@@ -40,8 +43,7 @@ mod test {
         let app = app();
 
         let mut headers = Headers::new();
-        headers.set_raw("X-Forwarded-For",
-                        vec![String::from("1.2.3.4").into_bytes()]);
+        headers.set_raw(X_FORWARD_FOR, vec![String::from("1.2.3.4").into_bytes()]);
         let res = request::get("http://localhost:3000/ip", headers, &app).unwrap();
 
         let result_body = response::extract_body_to_string(res);
