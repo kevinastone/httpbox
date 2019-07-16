0d2e84636b9638548e0e976ded942382af91e65f
Kevin Stone <kevinastone@gmail.com>
2019-07-16 17:02:53 -0500

    Bumped gotham to v0.4

--- src/test/request.rs
+++ src/test/request.rs
@@ -1,13 +1,14 @@
 #![cfg(test)]
 
-use gotham::test::TestRequest;
+use gotham::test::{Server, TestRequest};
 use headers::{Header, HeaderMapExt};
+use hyper::client::connect::Connect;
 
 pub trait TestRequestTypedHeader {
     fn with_typed_header<H: Header>(self, header: H) -> Self;
 }
 
-impl TestRequestTypedHeader for TestRequest<'_> {
+impl<S: Server, C: Connect> TestRequestTypedHeader for TestRequest<'_, S, C> {
     fn with_typed_header<H: Header>(mut self, header: H) -> Self {
         self.headers_mut().typed_insert(header);
         self
