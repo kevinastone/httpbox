9300cf65326c7e67fe1d4e07ba1ba095df717e98
Kevin Stone <kevinastone@gmail.com>
2022-01-29 13:45:58 -0600

    Migrate to Clap v3 with Derive macros for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use clap::{
-    app_from_crate, crate_authors, crate_description, crate_name, crate_version,
-};
-use clap::{value_t, value_t_or_exit, App, Arg, Error, ErrorKind, Shell};
+use clap::{App, IntoApp, Parser};
+use clap_complete::{generate, Generator, Shell};
 use hyper::Server;
 use std::io;
 use std::net::ToSocketAddrs;
@@ -17,43 +15,36 @@ mod service;
 #[cfg(test)]
 mod test;
 
-fn cli<'a, 'b>() -> App<'a, 'b> {
-    app_from_crate!()
-        .arg(
-            Arg::with_name("host")
-                .short("h")
-                .long("host")
-                .value_name("HOST")
-                .takes_value(true)
-                .default_value("0.0.0.0")
-                .env("HOSTNAME")
-                .help("Host address to listen on"),
-        )
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .value_name("PORT")
-                .takes_value(true)
-                .default_value("3000")
-                .env("PORT")
-                .help("Port to listen on"),
-        )
-        .arg(
-            Arg::with_name("threads")
-                .long("threads")
-                .value_name("THREADS")
-                .takes_value(true)
-                .help("Number of threads to process requests"),
-        )
-        .arg(
-            Arg::with_name("completions")
-                .long("completions")
-                .takes_value(true)
-                .value_name("SHELL")
-                .hidden(true)
-                .possible_values(&Shell::variants()),
-        )
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(
+        short,
+        long,
+        env,
+        default_value = "0.0.0.0",
+        help = "Host address to listen on"
+    )]
+    host: String,
+
+    #[clap(
+        short,
+        long,
+        env,
+        default_value_t = 3000,
+        help = "Port to listen on"
+    )]
+    port: u16,
+
+    #[clap(long, env, help = "Number of threads to process requests")]
+    threads: Option<usize>,
+
+    #[clap(long)]
+    completions: Option<Shell>,
+}
+
+fn print_completions<G: Generator>(gen: G, app: &mut App) {
+    generate(gen, app, app.get_name().to_string(), &mut io::stdout());
 }
 
 async fn shutdown_signal() {
@@ -64,34 +55,25 @@ async fn shutdown_signal() {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
 
-    let matches = cli().get_matches();
+    let args = Cli::parse();
 
-    if let Some(shell) = matches.value_of("completions") {
-        cli().gen_completions_to(
-            crate_name!(),
-            shell.parse::<Shell>()?,
-            &mut io::stdout(),
-        );
+    if let Some(generator) = args.completions {
+        let mut app = Cli::into_app();
+        print_completions(generator, &mut app);
         return Ok(());
     }
 
-    let host = matches.value_of("host").expect("Invalid host");
-    let port = value_t_or_exit!(matches.value_of("port"), u16);
-    let threads = match value_t!(matches.value_of("threads"), usize) {
-        Ok(val) => val,
-        Err(Error {
-            kind: ErrorKind::ArgumentNotFound,
-            ..
-        }) => ::num_cpus::get(),
-        Err(e) => e.exit(),
-    };
-
-    let addr = (host, port)
+    let threads = args.threads.unwrap_or_else(|| num_cpus::get());
+    let addr = (args.host.clone(), args.port)
         .to_socket_addrs()
         .ok()
         .and_then(|iter| iter.last())
         .unwrap_or_else(|| {
-            panic!("Invalid listening address: {}:{}", host, port)
+            panic!(
+                "Invalid listening address: {}:{}",
+                args.host.clone(),
+                args.port
+            )
         });
 
     let runtime = runtime::Builder::new_multi_thread()
