611a483546c9846fcdd1781318d99c4637045cbf
Kevin Stone <kevinastone@users.noreply.github.com>
2024-10-19 19:54:41 -0700

    Add /healthz healthcheck endpoint (#20)
    It merely returns "OK" with a 200 status code

---
+++ src/service/healthz.rs
@@ -0,0 +1,21 @@
+use crate::http::{ok, Request, Result};
+
+pub async fn healthz(_req: Request) -> Result {
+    ok("OK")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test::*;
+    use hyper::http::StatusCode;
+
+    #[tokio::test]
+    async fn test_healthz() {
+        let res = request().handle(healthz).await.unwrap();
+
+        assert_eq!(res.status(), StatusCode::OK);
+        let body = res.read_body_utf8().await.unwrap();
+        assert_eq!(body, "OK");
+    }
+}
